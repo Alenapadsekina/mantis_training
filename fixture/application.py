63f4a804146af3ee3fbd9e45bb2e0e2a2dc0455a
alena.padsekina <alena.padsekina@gmail.com>
2020-10-16 18:27:03 +0300

    soap

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.soap import SoapHelper
 
 class Application:
     def __init__(self, browser, base_url):
@@ -16,6 +17,7 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.base_url = base_url
+        self.soap = SoapHelper(self)
 
     def is_valid(self):
         try:
